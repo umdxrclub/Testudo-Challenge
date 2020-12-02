8750f1494e23a1c752794a8593a8f70e6f68c101
Sahil Mayenkar <sahil.mayenkar@gmail.com>
2020-12-01 19:43:22 -0500

    Set up menu manager

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI.Extensions;
+
+public class MainMenu : SimpleMenu<MainMenu>
+{
+    public override void OnBackPressed()
+    {
+        Application.Quit();
+    }
+}
