d36f0310b9581f1f523ae3b70dca7174e8d8f578
Sahil Mayenkar <sahil.mayenkar@gmail.com>
2020-12-01 19:54:10 -0500

    Added report bug feature

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,6 +5,18 @@ using UnityEngine.UI.Extensions;
 
 public class MainMenu : SimpleMenu<MainMenu>
 {
+    public void SendBugReportEmail()
+    {
+        string email = "umd.xr.club@gmail.com";
+        string subject = MyEscapeURL("Found bug in Testudo's Altar App");
+        Application.OpenURL("mailto:" + email + "?subject=" + subject);
+    }
+
+    private string MyEscapeURL(string url)
+    {
+        return UnityWebRequest.EscapeURL(url).Replace("+", "%20");
+    }
+
     public override void OnBackPressed()
     {
         Application.Quit();
