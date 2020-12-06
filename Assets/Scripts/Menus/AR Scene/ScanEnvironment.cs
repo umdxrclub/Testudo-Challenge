c58a4a8a0df78128d917499ccd08ee94e03a117f
Sahil Mayenkar <sahil.mayenkar@gmail.com>
2020-12-06 18:26:58 -0500

    Configured menu manager for AR Scene

---
+++ Assets/Scripts/Menus/AR Scene/ScanEnvironment.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UI.Extensions;
+using UnityEngine.XR.ARFoundation;
+
+public class ScanEnvironment : SimpleMenu<ScanEnvironment>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        GetComponent<UIManager>().arSessionOrigin = ((ARSessionOrigin) FindObjectOfType(typeof(ARSessionOrigin))).gameObject;
+        GetComponent<ARUXAnimationManager>().coachingOverlay = (ARKitCoachingOverlay) FindObjectOfType(typeof(ARKitCoachingOverlay));
+    }
+
+    public override void OnBackPressed()
+    {
+    }
+}
