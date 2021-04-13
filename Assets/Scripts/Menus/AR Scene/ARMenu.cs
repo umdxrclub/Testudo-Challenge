546ca7ac7dc91c887e9b4309d569061de5b5d323
Sahil Mayenkar <sahil.mayenkar@gmail.com>
2021-04-13 15:17:49 -0400

    Moved native share functionality into ARMenu script

--- Assets/Scripts/Menus/AR Scene/ARMenu.cs
+++ Assets/Scripts/Menus/AR Scene/ARMenu.cs
@@ -1,9 +1,41 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI.Extensions;
 
 public class ARMenu : SimpleMenu<ARMenu>
 {
-    public void LoadMainMenuScene()
+
+	public void TakeScreenshotAndShare()
+	{
+		StartCoroutine(TakeScreenshotAndShareCoroutine());
+	}
+
+    private IEnumerator TakeScreenshotAndShareCoroutine()
+    {
+        // Create a unique filename based on current time.
+        string timestamp = DateTime.Now.ToString("yyyyMMdd_hhmmss");
+        string fileName = "TestudoToGo_" + timestamp + ".png";
+        string filePath = Path.Combine(Application.persistentDataPath, fileName);
+
+        ScreenCapture.CaptureScreenshot(fileName);
+
+        // Screenshot capture and saving takes place in the background. Don't
+        // try to share until image has finished saving.
+        while(!File.Exists(filePath))
+        {
+            yield return null;
+        }
+
+        // Open iOS/Android native sharing popup.
+        string message = "Check out my virtual Testudo! - Created with Testudo-To-Go app. " +
+            "Download now on the App Store or Google Play Store";
+        new NativeShare().AddFile(filePath).SetText(message).Share();
+    }
+
+	public void LoadMainMenuScene()
     {
         SceneManager.LoadScene("MainMenu");
     }
